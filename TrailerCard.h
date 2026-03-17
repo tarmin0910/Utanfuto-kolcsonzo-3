1cc654666c58c4adb93018662e787abecda33184
baksaaron <baksa.aron@gmail.com>
2026-03-17 20:40:40 +0100

    Overload TrailerCard constructor to include imagePath

--- TrailerCard.h
+++ TrailerCard.h
@@ -1,12 +1,16 @@
-
 #pragma once
 #include <QFrame>
 #include <QString>
 
 class TrailerCard : public QFrame {
     Q_OBJECT
+
 public:
-    TrailerCard(const QString& title, const QString& pricePerDay, QWidget* parent=nullptr);
+    TrailerCard(const QString& title,
+                const QString& pricePerDay,
+                const QString& imagePath,
+                QWidget* parent = nullptr);
+
 signals:
     void orderClicked(const QString& title);
 };
